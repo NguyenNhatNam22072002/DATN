1946d5bab735b421ad0ee8598bc1e2a60479d167
NguyenNhatNam22072002 <nhatnam22072002@gmail.com>
2024-07-17 14:46:18 +0700

    Update: Loadmore for list product by category

--- lib/views/customer/categories/product_by_category.dart
+++ lib/views/customer/categories/product_by_category.dart
@@ -1,18 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:shoes_shop/constants/color.dart';
-import 'package:shoes_shop/resources/styles_manager.dart';
-import 'package:shoes_shop/views/customer/relational_screens/product_details.dart';
+
+import '../../../constants/color.dart';
 import '../../../models/category.dart';
 import '../../../models/product.dart';
 import '../../../resources/assets_manager.dart';
+import '../../../resources/styles_manager.dart';
 import '../../components/single_product_grid.dart';
 import '../../widgets/loading_widget.dart';
+import '../relational_screens/product_details.dart';
 
 class ProductByCategoryScreen extends StatefulWidget {
-  const ProductByCategoryScreen({super.key, required this.category});
+  const ProductByCategoryScreen({Key? key, required this.category})
+      : super(key: key);
 
   final Category category;
 
@@ -22,31 +23,39 @@ class ProductByCategoryScreen extends StatefulWidget {
 }
 
 class _ProductByCategoryScreenState extends State<ProductByCategoryScreen> {
+  int _productLimit = 8; // Initial limit of products to load
+  DocumentSnapshot? _lastDocument; // To store the last document snapshot
+
+  // Function to load more products
+  void _loadMoreProducts() {
+    setState(() {
+      _productLimit += 8; // Increase the product limit
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    // Stream to fetch products based on category and limit
     Stream<QuerySnapshot> productStream = FirebaseFirestore.instance
         .collection('products')
         .orderBy('uploadDate', descending: true)
         .where('isApproved', isEqualTo: true)
         .where('category', isEqualTo: widget.category.title)
+        .limit(_productLimit) // Limit number of products fetched
         .snapshots();
-
-    Size size = MediaQuery.sizeOf(context);
 
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        leading: Builder(
-          builder: (context) {
-            return GestureDetector(
-              onTap: () => Navigator.of(context).pop(),
-              child: const Icon(
-                Icons.chevron_left,
-                color: accentColor,
-                size: 35,
-              ),
-            );
-          },
+        leading: GestureDetector(
+          onTap: () => Navigator.of(context).pop(),
+          child: const Icon(
+            Icons.chevron_left,
+            color: accentColor,
+            size: 35,
+          ),
         ),
         actions: [
           Wrap(
@@ -131,33 +140,69 @@ class _ProductByCategoryScreenState extends State<ProductByCategoryScreen> {
               );
             }
 
-            return MasonryGridView.count(
-              crossAxisCount: 2,
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
-              padding: const EdgeInsets.only(
-                top: 0,
-              ),
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                final item = snapshot.data!.docs[index];
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
+                    ),
+                    padding: const EdgeInsets.only(top: 10),
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final DocumentSnapshot document =
+                          snapshot.data!.docs[index];
+                      final Product product = Product(
+                        prodId: document.id,
+                        vendorId: document[
+                            'vendorId'], // Example: Retrieve vendorId from document
+                        productName: document['productName'],
+                        price: document['price'],
+                        quantity: document['quantity'],
+                        category: document['category'],
+                        description: document['description'],
+                        scheduleDate: document['scheduleDate'].toDate(),
+                        isCharging: document['isCharging'],
+                        billingAmount: document['billingAmount'],
+                        brandName: document['brandName'],
+                        sizesAvailable:
+                            List<String>.from(document['sizesAvailable']),
+                        imgUrls: List<String>.from(document['imgUrls']),
+                        uploadDate: document['uploadDate'].toDate(),
+                        isFav: document['isFav'] ?? false,
+                        isApproved: document['isApproved'] ?? false,
+// Additional field
+                      );
 
-                Product product = Product.fromJson(item);
-
-                return InkWell(
-                  onTap: () => Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => ProductDetailsScreen(
-                        product: product,
-                      ),
+                      return InkWell(
+                        onTap: () => Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => ProductDetailsScreen(
+                              product: product,
+                            ),
+                          ),
+                        ),
+                        child: SingleProductGridItem(
+                          product: product,
+                          size: size,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                if (snapshot.data!.docs.length >= _productLimit)
+                  // Show load more button if more products can be loaded
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: _loadMoreProducts,
+                      child: Text('Load More'),
                     ),
                   ),
-                  child: SingleProductGridItem(
-                    product: product,
-                    size: size,
-                  ),
-                );
-              },
+              ],
             );
           },
         ),
